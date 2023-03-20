8b407308a48a35afb02e454f08c2966efcc14374
Lavinia Benvindo <110117509+laviniiabenvindo@users.noreply.github.com>
2023-03-20 18:30:45 -0300

    Add files via upload

---
+++ Atividade - Eventos Formulários/Atividade 03/app.js
@@ -0,0 +1,8 @@
+const btnConverter = document.getElementById('btnConverter')
+
+btnConverter.addEventListener('click', () => {
+    let inQuilo = document.getElementById('inQuilo');
+    let inGrama = document.getElementById('inGrama');
+    let outValor = document.getElementById('outValor');
+
+})
